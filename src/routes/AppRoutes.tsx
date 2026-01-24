38d79c012d3acf2e914f47bb748ce1a7d2503169
iu <doudou239@gmail.com>
2026-01-24 22:00:46 +0100

    Add AppRoutes with lazy-loaded pages for authentication and main features

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 
+import { ActivateAsync } from '@/pages/activate/Activate.async';
+import { AuthAsync } from '@/pages/auth/Auth.async';
+import { MainAsync } from '@/pages/main/index.async';
+
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<div>Home</div>} />
+      <Route path={'/signup'} element={<AuthAsync isRegistration={true} />} />
+      <Route path={'/login'} element={<AuthAsync />} />
+      <Route path={'/activate/:token'} element={<ActivateAsync />} />
+      <Route path={'/'} element={<MainAsync />} />
     </Routes>
   );
 };
