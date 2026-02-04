372513acbf187a8c731c5e2cf4ea2aea829e95ae
iu <doudou239@gmail.com>
2026-02-04 21:29:01 +0100

    Extend football API `models`.

--- src/pages/predictions/models/models.ts
+++ src/pages/predictions/models/models.ts
@@ -2,12 +2,17 @@ export interface FootballCompetitionsApi {
   competitions: FootballCompetitionApi[];
 }
 
-export interface FootballCompetitionApi {
+export interface FootballCompetitionBasicApi {
   id: number;
-  area: FootballAreaApi;
   name: string;
   code: string;
   type: string;
+  emblem: string;
+}
+
+/* /competitions */
+export interface FootballCompetitionApi extends FootballCompetitionBasicApi {
+  area: FootballAreaApi;
   // emblem: string;
   currentSeason: FootballSeasonApi;
   numberOfAvailableSeasons: number;
@@ -27,4 +32,46 @@ export interface FootballSeasonApi {
   endDate: string;
   currentMatchday: number;
   // winner: string | null;
+}
+
+export interface FootballTeamApi {
+  id: number;
+  name: string;
+  shortName: string;
+  tla: string;
+  crest: string; // URL
+}
+
+export interface FootballMatchTimeApi {
+  home: number | null;
+  away: number | null;
+}
+
+export interface FootballCompetitionMatchesApi {
+  competition: FootballCompetitionBasicApi;
+  matches: FootballMatchApi[];
+}
+
+export interface FootballMatchApi {
+  area?: FootballAreaApi;
+  competition: FootballCompetitionBasicApi;
+  season: FootballSeasonApi;
+  id: number;
+  utcDate: string;
+  status: 'FINISHED' | 'TIMED' | 'SCHEDULED' | 'POSTPONED' | 'SUSPENDED';
+  matchday: number;
+  stage: string;
+  group: string;
+  lastUpdated: string;
+  homeTeam: FootballTeamApi;
+  awayTeam: FootballTeamApi;
+  score: {
+    winner: null | 'HOME_TEAM' | 'AWAY_TEAM' | 'DRAW';
+    duration: string;
+    fullTime: FootballMatchTimeApi;
+    halfTime: FootballMatchTimeApi;
+    regularTime?: FootballMatchTimeApi;
+    extraTime?: FootballMatchTimeApi;
+    penalties?: FootballMatchTimeApi;
+  };
 }
