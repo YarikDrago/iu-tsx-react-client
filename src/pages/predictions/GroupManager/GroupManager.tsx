58d29f75a93942be6f4e8b68456e58ec631b2fc8
iu <doudou239@gmail.com>
2026-02-17 22:09:10 +0100

    Enhance: Add group name change, delete group, and invite code management functionality to `GroupManager`.

--- src/pages/predictions/GroupManager/GroupManager.tsx
+++ src/pages/predictions/GroupManager/GroupManager.tsx
@@ -37,10 +37,61 @@ const GroupManager = () => {
       );
       console.log(response);
       setSuccessCreation(true);
+      window.location.reload();
     } catch (e) {
       // TODO change
       console.error(e);
       setErrorMsg((e as Error).message);
+    }
+  }
+
+  async function changeGroupName() {
+    try {
+      setErrorMsg('');
+      appData.showLoader();
+      const response = await universalFetchRequest(
+        `tournaments/groups/${store.id}`,
+        HTMLRequestMethods.PATCH,
+        {
+          name: store.name,
+        }
+      );
+      console.log(response);
+      window.location.reload();
+    } catch (e) {
+      setErrorMsg((e as Error).message);
+    } finally {
+      appData.hideLoader();
+    }
+  }
+
+  async function deleteGroup() {
+    try {
+      appData.showLoader();
+      setErrorMsg('');
+      await universalFetchRequest(`tournaments/groups/${store.id}`, HTMLRequestMethods.DELETE, {});
+      window.location.reload();
+    } catch (e) {
+      setErrorMsg((e as Error).message);
+    } finally {
+      appData.hideLoader();
+    }
+  }
+
+  async function changeInviteCode() {
+    try {
+      appData.showLoader();
+      setErrorMsg('');
+      await universalFetchRequest(
+        `tournaments/groups/${store.id}/invite-code`,
+        HTMLRequestMethods.PATCH,
+        {}
+      );
+      window.location.reload();
+    } catch (e) {
+      setErrorMsg((e as Error).message);
+    } finally {
+      appData.hideLoader();
     }
   }
 
@@ -72,7 +123,7 @@ const GroupManager = () => {
                 store.name = e.target.value;
               }}
             />
-            {store.isNew && (
+            {store.isNew ? (
               <button
                 onClick={(e) => {
                   e.preventDefault();
@@ -82,6 +133,52 @@ const GroupManager = () => {
               >
                 Create
               </button>
+            ) : (
+              <>
+                <div style={{ display: 'flex', gap: '10px' }}>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      console.log('invite code:', store.inviteCode);
+                      navigator.clipboard.writeText(
+                        `${process.env.BASE_URL}/predictions/groups/join/?code=${store.inviteCode}`
+                      );
+                      alert('Invite link was copied to clipboard');
+                    }}
+                  >
+                    Invite link
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      changeInviteCode();
+                    }}
+                  >
+                    Change invite code
+                  </button>
+                </div>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    changeGroupName();
+                  }}
+                  disabled={store.name === store.oldName}
+                >
+                  Change name
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    deleteGroup();
+                  }}
+                >
+                  Delete group
+                </button>
+              </>
             )}
             {errorMsg !== '' && <p className={styles.error}>{errorMsg}</p>}
           </>
