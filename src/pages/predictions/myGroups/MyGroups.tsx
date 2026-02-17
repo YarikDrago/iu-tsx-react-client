377e54959d8e067d72d0b85937a6f10e089a1789
iu <doudou239@gmail.com>
2026-02-17 22:10:36 +0100

    Enhance: Add `manageGroup` functionality to `MyGroups` for handling group management details via `Group` DTO; update table structure for owners.

--- src/pages/predictions/myGroups/MyGroups.tsx
+++ src/pages/predictions/myGroups/MyGroups.tsx
@@ -4,6 +4,7 @@ import appData from '@/app.data';
 import { universalFetchRequest } from '@/function/api/universalFetchRequest';
 import { HTMLRequestMethods } from '@/models/htmlRequestMethods';
 import { Competition } from '@/pages/predictions/models/competition.dto';
+import { Group } from '@/pages/predictions/models/group.dto';
 import { GroupMember } from '@/pages/predictions/models/groupMember.dto';
 import { Season } from '@/pages/predictions/models/season.dto';
 import { useRequireAccessToken } from '@/shared/hooks/useRequireAccessToken';
@@ -48,6 +49,34 @@ const MyGroups = () => {
     }
   }
 
+  async function manageGroup(group: GroupSummary) {
+    try {
+      setErrorMsg('');
+      appData.showLoader();
+      // TODO download Competition of current group and save it to prevent reloading from DB
+
+      const groupInfo = await universalFetchRequest<Group>(
+        `tournaments/groups/${group.id}`,
+        HTMLRequestMethods.GET,
+        {}
+      );
+      console.log(groupInfo);
+      appData.group.show({
+        isNew: false,
+        id: groupInfo.id,
+        oldName: groupInfo.name,
+        name: groupInfo.name,
+        competition: groupInfo.tournament,
+        season: groupInfo.season,
+        inviteCode: groupInfo.inviteCode,
+      });
+    } catch (e) {
+      setErrorMsg((e as Error).message);
+    } finally {
+      appData.hideLoader();
+    }
+  }
+
   return (
     <div>
       <p>My Groups</p>
@@ -69,17 +98,18 @@ const MyGroups = () => {
               <tr key={group.id}>
                 <td>{String(group.id)}</td>
                 <td>{group.name}</td>
-                <td>{group.isOwner ? 'Yes' : 'No'}</td>
                 <td>{group.tournament.name}</td>
                 <td>
                   {group.season.start_date} - {group.season.end_date}
                 </td>
+                <td>{group.isOwner ? 'Yes' : 'No'}</td>
                 <td>
                   {group.isOwner ? (
                     <div className={styles.actionBlock}>
                       <button
                         onClick={(e) => {
                           e.stopPropagation();
+                          manageGroup(group);
                         }}
                       >
                         Manage
