92b5185b4ea08d592ba121244ad143ad7f8d8b7a
iu <doudou239@gmail.com>
2026-02-17 22:07:56 +0100

    Enhance: Add `tournament` and `season` details to `MyGroups` table; update `Group` DTO with new fields.

--- src/pages/predictions/myGroups/MyGroups.tsx
+++ src/pages/predictions/myGroups/MyGroups.tsx
@@ -3,6 +3,9 @@ import React, { useEffect } from 'react';
 import appData from '@/app.data';
 import { universalFetchRequest } from '@/function/api/universalFetchRequest';
 import { HTMLRequestMethods } from '@/models/htmlRequestMethods';
+import { Competition } from '@/pages/predictions/models/competition.dto';
+import { GroupMember } from '@/pages/predictions/models/groupMember.dto';
+import { Season } from '@/pages/predictions/models/season.dto';
 import { useRequireAccessToken } from '@/shared/hooks/useRequireAccessToken';
 
 import * as styles from './MyGroups.module.scss';
@@ -11,6 +14,11 @@ export interface GroupSummary {
   id: number;
   name: string;
   isOwner: boolean;
+  createdAt: string;
+  inviteCode: string;
+  tournament: Competition;
+  season: Season;
+  members: GroupMember[];
 }
 
 const MyGroups = () => {
@@ -50,6 +58,8 @@ const MyGroups = () => {
             <tr>
               <th>ID</th>
               <th>Name</th>
+              <th>Tournament</th>
+              <th>Season</th>
               <th>Is owner</th>
               <th>Action</th>
             </tr>
@@ -60,6 +70,10 @@ const MyGroups = () => {
                 <td>{String(group.id)}</td>
                 <td>{group.name}</td>
                 <td>{group.isOwner ? 'Yes' : 'No'}</td>
+                <td>{group.tournament.name}</td>
+                <td>
+                  {group.season.start_date} - {group.season.end_date}
+                </td>
                 <td>
                   {group.isOwner ? (
                     <div className={styles.actionBlock}>
