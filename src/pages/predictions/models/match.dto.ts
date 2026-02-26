6bd8b7cb1232510b127a097fca744adb0e81bbdd
iu <doudou239@gmail.com>
2026-02-26 13:58:33 +0100

    Enhance: Add `MatchStatus` enum and update `MatchDto` status field to improve type safety in predictions module.

--- src/pages/predictions/models/match.dto.ts
+++ src/pages/predictions/models/match.dto.ts
@@ -1,6 +1,17 @@
 import { Competition } from '@/pages/predictions/models/competition.dto';
 import { Season } from '@/pages/predictions/models/season.dto';
 
+export enum MatchStatus {
+  SCHEDULED = 'SCHEDULED',
+  POSTPONED = 'POSTPONED',
+  SUSPENDED = 'SUSPENDED',
+  TIMED = 'TIMED',
+  IN_PLAY = 'IN_PLAY',
+  FINISHED = 'FINISHED',
+  // CANCELLED = 'CANCELLED',
+  // AWARDED = 'AWARDED',
+}
+
 export interface MatchDto {
   id: number;
   external_id: string;
@@ -11,7 +22,7 @@ export interface MatchDto {
   home_team: string | null;
   away_team: string | null;
   start_time: string | null;
-  status: string;
+  status: MatchStatus;
   home_score: number | null;
   away_score: number | null;
   updated_at: string;
